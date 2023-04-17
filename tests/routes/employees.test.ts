2b54be05aa80ae4db4b57e7b07e0c2e3b9372a56
DJPajares <dj.pajares@gmail.com>
2023-04-18 00:24:29 +0800

    test: add post and get employees

--- tests/routes/employees.test.ts
+++ tests/routes/employees.test.ts
@@ -1,38 +1,90 @@
 import request from 'supertest';
 import app from '../../src/index';
 
-const employees = [
-  {
-    id: '1',
-    login: 'test1',
-    name: 'Test 1',
-    salary: '1000'
-  },
-  {
-    id: '2',
-    login: 'test2',
-    name: 'Test 2',
-    salary: '2000'
-  }
-];
-
 describe('Employees API', () => {
   describe('POST /employees', () => {
-    it('should return 200 OK', async () => {
-      const res = await request(app)
-        .post('/api/employees')
-        .send({ csvData: employees });
+    it('should upload a CSV file and create or update employees', async () => {
+      const csvData =
+        'id,login,name,salary\n1,alice,Alice,30\n2,bob,Bob,35\n3,charlie,Charlie,40';
 
-      expect(res.status).toEqual(200);
-      expect(res.body).toEqual(employees);
+      const response = await request(app)
+        .post('/api/employees')
+        .attach('csv', Buffer.from(csvData), { filename: 'employees.csv' });
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ success: true });
+    });
+
+    it('should return status 400 if CSV file is missing', async () => {
+      const response = await request(app).post('/api/employees');
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return status 400 if CSV file have lesser number of columns', async () => {
+      const csvData = 'id,login,name,salary\n1,alice,Alice\n2,bob,Bob,35';
+
+      const response = await request(app)
+        .post('/api/employees')
+        .attach('csv', Buffer.from(csvData), { filename: 'employees.csv' });
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return status 400 if CSV file have more number of columns', async () => {
+      const csvData = 'id,login,name,salary\n1,alice,Alice,50,25\n2,bob,Bob,35';
+
+      const response = await request(app)
+        .post('/api/employees')
+        .attach('csv', Buffer.from(csvData), { filename: 'employees.csv' });
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return status 400 if CSV file have invalid incorrectly formatted salaries', async () => {
+      const csvData = 'id,login,name,salary\n1,alice,Alice,five\n2,bob,Bob,35';
+
+      const response = await request(app)
+        .post('/api/employees')
+        .attach('csv', Buffer.from(csvData), { filename: 'employees.csv' });
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return status 400 if CSV file have a salary value of less than 0', async () => {
+      const csvData = 'id,login,name,salary\n1,alice,Alice,-50\n2,bob,Bob,35';
+
+      const response = await request(app)
+        .post('/api/employees')
+        .attach('csv', Buffer.from(csvData), { filename: 'employees.csv' });
+
+      expect(response.status).toBe(400);
     });
   });
 
   describe('GET /employees', () => {
-    it.only('should return 200 OK', async () => {
-      const res = await request(app).get('/api/employees');
+    it('should return status 200 if all request params are present', async () => {
+      const res = await request(app).get(
+        '/api/employees?minSalary=0&maxSalary=4000&offset=0&limit=30&sort=+name'
+      );
 
       expect(res.status).toEqual(200);
+    });
+
+    it('should return status 400 if any of the request params is missing', async () => {
+      const res = await request(app).get(
+        '/api/employees?maxSalary=4000&offset=0&limit=30&sort=+name'
+      );
+
+      expect(res.status).toEqual(400);
+    });
+
+    it('should return status 400 if any of the request params data format is invalid', async () => {
+      const res = await request(app).get(
+        '/api/employees?minSalary=0&maxSalary=4000&offset=0&limit=abc&sort=+name'
+      );
+
+      expect(res.status).toEqual(400);
     });
   });
 });
